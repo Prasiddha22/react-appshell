54aade9645f6d09e4db05d1fe834142851e321b8
prasiddha22 <prasiddhaneupane22@gmail.com>
2022-07-10 08:27:16 +0545

    Navbar code changes

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -1,5 +1,35 @@
-import React from 'react';
+import React, { useImperativeHandle, useRef } from 'react';
 
-export const Navbar = () => {
-  return <div>Navbar</div>;
+type Props = {
+  navContent: React.ReactNode;
 };
+
+type NavHandle = {
+  getNavHeight: () => number;
+};
+
+const Navbar: React.ForwardRefRenderFunction<NavHandle, Props> = (
+  { navContent },
+  ref
+) => {
+  const navRef = useRef<HTMLDivElement>(null);
+
+  useImperativeHandle(ref, () => {
+    return {
+      getNavHeight: () => {
+        return navRef.current?.clientHeight || 0;
+      },
+    };
+  });
+
+  return (
+    <div
+      ref={navRef}
+      style={{ padding: '10px 10px', position: 'sticky', top: 0 }}
+    >
+      Navbar
+    </div>
+  );
+};
+
+export default React.forwardRef(Navbar);
