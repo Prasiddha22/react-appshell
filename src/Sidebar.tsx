0775949c70a696ca439e32773872487364c1848a
prasiddha22 <prasiddhaneupane22@gmail.com>
2022-07-10 08:26:48 +0545

    Sidebar code changes

--- src/Sidebar.tsx
+++ src/Sidebar.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
 
-export const Sidebar = () => {
+type Props = {
+  style: React.CSSProperties;
+};
+
+export const Sidebar = ({ style }: Props) => {
+  const top = style?.top ?? 0;
   return (
     <div
-      style={{ width: '20vw', height: '100vh', background: '#00000050' }}
+      style={{
+        width: '20vw',
+        background: '#00000050',
+        position: 'sticky',
+        left: 0,
+        top,
+        height: `calc(100vh-${top}px)`,
+      }}
     ></div>
   );
 };
